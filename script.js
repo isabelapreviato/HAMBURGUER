31bfefdfb6f5be5bf232ad5fd3af12f207b25e65
isabelapreviato <isabelapreviato@hotmail.com>
2019-02-14 12:09:02 -0200

    primeiro

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ const menuHamburguer = document.querySelector('menu-hamburguer');
 const navMenuHamburguer = document.querySelector('.nav-menu-hamburguer');
 
 const atualizarMenu = function() {
+    menuHamburguer.classList.toggle('ativo');
     navMenuHamburguer.classList.toggle ('visivel');
 }
+
+
 
 menuHamburguer.onclick = atualizarMenu;
